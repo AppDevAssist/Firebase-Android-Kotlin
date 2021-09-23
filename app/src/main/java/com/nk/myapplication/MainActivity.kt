9c887eaee5751d42c96fc06342d6839c37a8c64e
n-k-d <n.k.d.assist@gmail.com>
2021-09-23 17:28:40 +0530

    realtime database

--- app/src/main/java/com/nk/myapplication/MainActivity.kt
+++ app/src/main/java/com/nk/myapplication/MainActivity.kt
@@ -2,10 +2,40 @@ package com.nk.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message2")
+        val tv = findViewById<TextView>(R.id.textView)
+        myRef.setValue("Hello, World 2!")
+
+        // Read from the database
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                tv.text = value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("MainActivity", "Failed to read value.", error.toException())
+            }
+
+        })
     }
 }
