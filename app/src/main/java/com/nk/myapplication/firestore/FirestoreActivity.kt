6774f4a6db90447a4ffe177fa45976da8943bc8f
NKD <n.k.d.assist@gmail.com>
2021-11-29 19:33:50 +0530

    Firestore add data hashmap

--- app/src/main/java/com/nk/myapplication/firestore/FirestoreActivity.kt
+++ app/src/main/java/com/nk/myapplication/firestore/FirestoreActivity.kt
@@ -2,10 +2,14 @@ package com.nk.myapplication.firestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.nk.myapplication.databinding.ActivityFirestoreBinding
 
 class FirestoreActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFirestoreBinding
+    val firestore = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +17,21 @@ class FirestoreActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        binding.buttonAdd.setOnClickListener{
+            val userInfo = hashMapOf(
+                "name" to binding.etName.text.toString(),
+                "email" to binding.etEmail.text.toString(),
+                "updates" to binding.updateswitch.isChecked,
+                "age" to binding.etAge.text.toString().toIntOrNull()
+            )
 
-
-
+            firestore.collection("subscribers").document(binding.etEmail.text.toString()).set(userInfo)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Subscriber added", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener{
+                    Toast.makeText(this, "Failed !!", Toast.LENGTH_LONG).show()
+                }
+        }
 
     }
 }
